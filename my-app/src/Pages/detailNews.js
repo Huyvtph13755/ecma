916509e1b851efd4fe15927bee5d57feaed64d45
Huyvtph13755 <93895164+Huyvtph13755@users.noreply.github.com>
2022-01-13 12:24:38 +0700

    up

--- my-app/src/Pages/detailNews.js
+++ my-app/src/Pages/detailNews.js
@@ -5,8 +5,8 @@ const DetailNewsPage = {
         const found = data.find((element) => element.id === id);
 
         return `
-        <div class="mx-auto">
-            <h1>${found.title}</h1>
+        <div class="mx-auto max-w-screen-lg font-sans">
+            <h1 class="font-bold text-cyan-900 text-xl my-6">${found.title}</h1>
             <img class="w-full" src="${found.img}" />
             <p>${found.desc}</p>
         </div>
---
+++ my-app/src/components/banner.js
@@ -0,0 +1,9 @@
+const Banner = {
+    render() {
+        return /* html */ `<div class="max-w-screen-lg m-auto font-sans">
+        <div class="mt-2">
+        <a href=""><img class="w-full" src="https://picsum.photos/1000/400" alt=""></a>
+      </div>`;
+    },
+};
+export default Banner;
